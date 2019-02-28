786621ed420913facf9fda8a60d9bf4b20dae62f
Dennis Shar <dennis.shar@servicenow.com>
2019-02-28 15:48:13 +0200

    sliding menu added

--- CustomStocks/app/src/main/java/com/celsius/customstocks/MainActivity.java
+++ CustomStocks/app/src/main/java/com/celsius/customstocks/MainActivity.java
@@ -1,9 +1,12 @@
 package com.celsius.customstocks;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import com.celsius.customstocks.custombehaviour.BottomNavigationViewBehavior;
 import com.celsius.customstocks.fragments.EarningCalendarFragment;
@@ -15,7 +18,9 @@ import com.celsius.customstocks.fragments.StocksFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 
@@ -25,10 +30,21 @@ public class MainActivity extends BaseActivity {
     private BottomNavigationView bottomNavigationView;
     private FrameLayout fragmentViewPlaceholder;
 
+    private DrawerLayout drawerLayout;
+    private LinearLayout content;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ///////////   Sliding menu configuration ////////////
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        content = (LinearLayout) findViewById(R.id.content);
+        setActionBarDrawerToggle();
+        ///////////////////////////////////////////////////////////
+
 
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
 
@@ -78,6 +94,27 @@ public class MainActivity extends BaseActivity {
     protected void onResume() {
         super.onResume();
         bottomNavigationView.setSelectedItemId(R.id.action_item3);
+    }
+
+    private void setActionBarDrawerToggle(){
+        drawerLayout.setScrimColor(Color.TRANSPARENT);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close) {
+            private float scaleFactor = 6f;
+
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                super.onDrawerSlide(drawerView, slideOffset);
+                float slideX = drawerView.getWidth() * slideOffset;
+                content.setTranslationX(slideX);
+                content.setScaleX(1 - (slideOffset / scaleFactor));
+                content.setScaleY(1 - (slideOffset / scaleFactor));
+            }
+        };
+
+        drawerLayout.setScrimColor(Color.TRANSPARENT);
+        drawerLayout.setDrawerElevation(0f);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
     }
 
 }
